51a97a5c973c0c7b8e8e987d8cf2bfad941e273e
Chris <cmangotti3@gmail.com>
2023-04-26 15:03:40 -0400

    starting point

---
+++ PointOfSaleTerminal/PointOfSaleTerminal/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSaleTerminal
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public static List<Product> ProductsList { get; set; } = new List<Product>();
+        public Product(string Name, string Category, string Description, decimal Price)
+        {
+            this.Name = Name;
+            this.Category = Category;
+            this.Description = Description;
+            this.Price = Price;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0,-15} {1,-18} {2,-30} {3,10}", $"{Name}", $"{Category}", $"{Description}", $"${Price}");
+        }
+        public static void PrintProducts()
+        {
+            for (int i = 0; i < ProductsList.Count; i++)
+            {
+                Console.WriteLine(i+1+ ".\t" +ProductsList[i].ToString());
+            }
+        }
+        
+    }
+}
