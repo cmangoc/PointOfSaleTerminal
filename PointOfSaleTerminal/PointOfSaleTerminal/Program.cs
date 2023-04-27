de254e02a01978d67c674ebbcecadb6a271cd80e
Chris <cmangotti3@gmail.com>
2023-04-27 09:21:08 -0400

    Added base functions

--- PointOfSaleTerminal/PointOfSaleTerminal/Program.cs
+++ PointOfSaleTerminal/PointOfSaleTerminal/Program.cs
@@ -1,23 +1,34 @@
-﻿namespace PointOfSaleTerminal
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PointOfSaleTerminal
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Product.ProductsList.Add(new Product("Bread", "Food", "wholegrain", 4.99m));
-            Product.ProductsList.Add(new Product("Chips", "Food", "Potatoc chips", 5.99m));
-            Product.ProductsList.Add(new Product("SweetPotato", "Food", "Fesh sweet potato", 0.99m));
-            Product.ProductsList.Add(new Product("Beef", "Food", "Chunk roasted beef, 4pound", 35.00m));
-            Product.ProductsList.Add(new Product("Notebook", "Office Supplies", "1 Subject Notebook", 0.99m));
-            Product.ProductsList.Add(new Product("Binder", "Office Supplies", "3-ringed binder", 4.69m));
-            Product.ProductsList.Add(new Product("Pencils", "Office Supplies", "30 Pre-Sharpened, #2 Pencils", 8.98m));
-            Product.ProductsList.Add(new Product("Black Pen", "Office Supplies", "It's a really good pen", 449.99m));
-            Product.ProductsList.Add(new Product("Lacrosse Stick", "Sporting Goods", "Netted stick for playing game", 99.88m));
-            Product.ProductsList.Add(new Product("Basketball", "Sporting Goods", "Always round guaranteed", 15.65m));
-            Product.ProductsList.Add(new Product("Horseshoes", "Sporting Goods", "Not for Horses", 44.95m));
-            Product.ProductsList.Add(new Product("Shorts", "Sporting Goods", "For the true sportsperson", 19.98m));
+            Product.ProductsList.Add(new Product("Bread", "Food", "wholegrain", 4.99m, 1));
+            Product.ProductsList.Add(new Product("Chips", "Food", "Potato chips", 5.99m, 1));
+            Product.ProductsList.Add(new Product("SweetPotato", "Food", "Fresh sweet potato", 0.99m, 1));
+            Product.ProductsList.Add(new Product("Beef", "Food", "Chunk roasted beef, 4pound", 35.00m, 1));
+            Product.ProductsList.Add(new Product("Notebook", "Office Supplies", "1 Subject Notebook", 0.99m, 1));
+            Product.ProductsList.Add(new Product("Binder", "Office Supplies", "3-ringed binder", 4.69m, 1));
+            Product.ProductsList.Add(new Product("Pencils", "Office Supplies", "30 Pre-Sharpened, #2 Pencils", 8.98m, 1));
+            Product.ProductsList.Add(new Product("Black Pen", "Office Supplies", "It's a really good pen", 449.99m, 1));
+            Product.ProductsList.Add(new Product("Lacrosse Stick", "Sporting Goods", "Netted stick for playing game", 99.88m, 1));
+            Product.ProductsList.Add(new Product("Basketball", "Sporting Goods", "Always round guaranteed", 15.65m, 1));
+            Product.ProductsList.Add(new Product("Horseshoes", "Sporting Goods", "Not for Horses", 44.95m, 1));
+            Product.ProductsList.Add(new Product("Shorts", "Sporting Goods", "For the true sportsperson", 19.98m, 1));
 
             Product.PrintProducts();
+
+            bool goOn = true;
+            while (goOn)
+            {
+                Product.OrderProduct();
+                goOn = Validator.Continue();
+            }
+            decimal grandTotal = Product.DisplayReceipt();
+            Product.PayReceipt(grandTotal);
         }
     }
 }
---
+++ PointOfSaleTerminal/PointOfSaleTerminal/Validator.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSaleTerminal
+{
+    public class Validator
+    {
+        public static int ValidateInt()
+        {
+            try
+            {
+                int input = int.Parse(Console.ReadLine());
+                return input;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input, NaN.");
+                return ValidateInt();
+            }
+        }
+        public static decimal ValidateDecimal()
+        {
+            try
+            {
+                decimal input = decimal.Parse(Console.ReadLine());
+                return Math.Round(input,2);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input, NaN.");
+                return ValidateInt();
+            }
+        }
+        public static bool Continue()
+        {
+            Console.WriteLine("Do you want to order something else? (y/n)");
+            string input = Console.ReadLine().Trim().ToLower();
+            if (input == "y")
+            {
+                return true;
+            }
+            else if (input == "n")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, try agian.");
+                return Continue();
+            }
+        }
+        public static string ValidateCreditCard()
+        {
+            Console.WriteLine("Enter a 16 digit credit card number: ");
+            string cardNumber = "";
+            string input = Console.ReadLine().Trim();
+            if (input.Length == 16)
+            {
+                char[] chars = input.ToCharArray();
+                foreach(char c in chars)
+                {
+                    if (char.IsNumber(c))
+                    {
+                        cardNumber += c;
+                    }
+                }
+            }
+            else
+            {
+                return ValidateCreditCard();
+            }
+            if (cardNumber.Length == 16)
+            {
+                cardNumber = "****-****-****-" + cardNumber.Substring(12);
+                return cardNumber;
+            }
+            else
+            {
+                return ValidateCreditCard();
+            }
+        }
+        public static int ValidateMonth()
+        {
+            Console.WriteLine("Enter the expiration month: ");
+            int month = ValidateInt();
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+            else
+            {
+                return ValidateMonth();
+            }
+        }
+        public static int ValidateYear()
+        {
+            Console.WriteLine("Enter the expiration year: ");
+            int year = ValidateInt();
+            if (year >= 23 && year <= 99)
+            {
+                return year;
+            }
+            else
+            {
+                return ValidateYear();
+            }
+        }
+        public static string ValidateCVV()
+        {
+            Console.WriteLine("Enter a 3 digit CVV: ");
+            string numCVV = "";
+            string input = Console.ReadLine().Trim();
+            if (input.Length == 3)
+            {
+                char[] chars = input.ToCharArray();
+                foreach (char c in chars)
+                {
+                    if (char.IsNumber(c))
+                    {
+                        numCVV += c;
+                    }
+                }
+            }
+            else
+            {
+                return ValidateCreditCard();
+            }
+            if (numCVV.Length == 3)
+            {
+                return numCVV;
+            }
+            else
+            {
+                return ValidateCreditCard();
+            }
+        }
+        public static string ValidateCheck()
+        {
+            Console.WriteLine("Enter a 9 digit Routing Number: ");
+            string routingNum = "";
+            string input = Console.ReadLine().Trim();
+            if (input.Length == 9)
+            {
+                char[] chars = input.ToCharArray();
+                foreach (char c in chars)
+                {
+                    if (char.IsNumber(c))
+                    {
+                        routingNum += c;
+                    }
+                }
+            }
+            else
+            {
+                return ValidateCreditCard();
+            }
+            if (routingNum.Length == 9)
+            {
+                return routingNum;
+            }
+            else
+            {
+                return ValidateCheck();
+            }
+        }
+    }
+}
