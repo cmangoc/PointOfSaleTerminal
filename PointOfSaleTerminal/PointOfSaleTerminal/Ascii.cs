5d75fe79bf6bfcb877e40740219dc7fb97d5265a
Mikael Addae <mikael.addae@gmail.com>
2023-04-28 11:52:31 -0400

    Ascii Class Functional

--- PointOfSaleTerminal/PointOfSaleTerminal/Ascii.cs
+++ PointOfSaleTerminal/PointOfSaleTerminal/Ascii.cs
@@ -18,56 +18,23 @@ namespace PointOfSaleTerminal
             this.AsciiName = Ascii;
         }
 
-        public static void PrintAscii()
+        public static void PrintAscii(int input)
         {
-            for (int i = 0; i < 84; i++)
-            {
-                Console.Write("=");
-            }
-            Console.WriteLine();
+
+
             for (int i = 0; i < AsciiProductsList.Count; i++)
             {
-                Console.WriteLine(i + 1 + ".\t" + AsciiProductsList[i].ToString());
+                if (i == input)
+                    Console.WriteLine(AsciiProductsList[i].ToString());
+
             }
-            for (int i = 0; i < 84; i++)
-            {
-                Console.Write("=");
-            }
-            Console.WriteLine();
+
         }
 
         public override string ToString()
         {
             return String.Format("{0,-70} {1, 10}", $"{AsciiName}", $"${Price}");
-            //return (AsciiName + Price);
+
         }
     }
 }
-
-
-        //public string AsciiName { get; set; }
-
-        //public Ascii(string Ascii, decimal Price) : base("", "", "", Price)
-        //{
-        //    this.AsciiName = Ascii;
-        //}
-        
-
-        //public override string ToString()
-        //{
-        //    return String.Format("{0,-60} {3,10}", $"{AsciiName}", $"${Price}");
-        //}
-
-
-
-        //List<Ascii> AsciiList { get; set; } = new List<Ascii>();
-        //public int AsciiChoice { get; set; }
-        //public Ascii(int asciiChoice)
-        //{
-        //    this.AsciiChoice = asciiChoice;
-        //}
-
-        //public void AsciiTime (int asciiTime)
-        //{
-        //    Console.WriteLine(AsciiList.IndexOf(AsciiList[asciiTime]));
-        //}
